8b90a71126139c9fd4b61288aedc73280a33f570
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-04-23 16:14:30 +0200

    update jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import user from '../data/user.json';
-import { Profile } from './Profile/Profile';
-//
+import statistics from '../data/statistics.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+import { Profile } from 'components/Profile/Profile';
+import { FriendList } from 'components/FriendList/FriendList';
+import { Statistics } from 'components/Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -21,6 +27,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={statistics} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
